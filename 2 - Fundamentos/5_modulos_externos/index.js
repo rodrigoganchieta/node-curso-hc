1dfc2ab80cb2eb7b442ece06c4c7e3517ef23133
Rodrigo Anchieta <rodrigoganchieta@gmail.com>
2023-12-28 12:35:53 -0300

    first commit

---
+++ 2 - Fundamentos/5_modulos_externos/index.js
@@ -0,0 +1,12 @@
+// --nome=Rodrigo Anchieta --idade=33
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2));
+
+console.log(args);
+
+const nome = args["nome"];
+const idade = args["idade"];
+
+console.log(nome);
+console.log(idade);
